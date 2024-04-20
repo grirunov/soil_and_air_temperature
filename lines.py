feb9ed51a1e5e6f98c88ae884ac0647a1edf2490
grirunov <grirunov@gmail.com>
2024-04-20 18:47:38 +0400

    version 3 (colors), works

--- lines.py
+++ lines.py
@@ -3,11 +3,18 @@ import numpy as np
 data = np.loadtxt("data/soil_average.csv", delimiter=",")
 import matplotlib.pyplot as plt
 
-plt.plot(data[0,:],"b*-")
+#for x in data:
+    #print("x = ", x)
+    #plt.plot(x)
 #plt.plot(data[1,:])
 #plt.plot(data[2,:])
 #plt.plot(data[3,:])
 #plt.plot(data[4,:])
 
-plt.show()
+#plt.show()
+colors = ["r","m","y","0.2","g"]
+print(data.shape)
+for i in range (data.shape[0]):
+    plt.plot(data[i,:],colors[i])
 
+plt.show()
