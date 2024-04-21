4015702440e10e35332c68e6d9c87cf988b3125d
grirunov <grirunov@gmail.com>
2024-04-21 16:19:06 +0400

    version 5 (works!!)

--- lines.py
+++ lines.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 #plt.show()
 
 heights = (25,117,535,1070,1700)
-import statistics as st
 
 #line1 = data[0,:]
 #line2 = data[1,:]
@@ -21,19 +20,28 @@ import statistics as st
 #a = statistics.mean(line_diff)
 #print(a/(heights[1]-heights[0]))
 labels_a = ("Sam-Zug","Zug-Tqi","Tqi-Pas","Pas-Bak")
+xpoints = np.linspace(1,12,num=12)
+c = np.zeros((12))
 
 for j in range (data.shape[0]-1):
     line1 = data[j,:]
     line2 = data[j+1,:]
     line_diff = line1-line2
-    b = line_diff/(heights[1]-heights[0])
+    b = line_diff/(heights[j+1]-heights[j])*100
     #print(st.mean(b))
     #for k in range (b.shape[0]):
-    plt.plot(b,label=labels_a[j])
-def months_and_meters():
-        plt.ylabel("deg/m")
+    plt.plot(xpoints,b,label=labels_a[j])
+    c += b
+    #print("x= ",b)
+def months_and_100meters():
+        plt.ylabel("deg/100m")
+        plt.xlabel("Months")
         plt.legend()
-        plt.xticks(np.arange(12))
+        plt.xticks(np.arange(13))
         plt.grid(axis="x")
-        plt.show()
-months_and_meters()
+#months_and_100meters()
+c /= (data.shape[0]-1)
+plt.plot(xpoints,c,"k")
+months_and_100meters()
+print(np.mean(c))
+plt.show()
